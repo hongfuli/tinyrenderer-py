44b5ef6a2437b1b1d008cd187bb5313dba62861a
hongfuli <tony.li.fly@gmail.com>
2022-04-28 23:22:46 +0800

    final draw line

--- line.py
+++ line.py
@@ -7,4 +7,81 @@ def line_1(x0, y0, x1, y1: int, image: CoordinateImage, color):
         x = int(x0 * (1 - t) + x1 * t)
         y = int(y0 * (1 - t) + y1 * t)
         image.set(x, y, color)
-        t += 0.1
+        t += 0.01
+
+
+def line_2(x0, y0, x1, y1: int, image: CoordinateImage, color):
+    for x in range(x0, x1 + 1):
+        t = (x - x0) / (x1 - x0)
+        y = int(y0 * (1 - t) + y1 * t)
+        image.set(x, y, color)
+
+
+def line_3(x0, y0, x1, y1: int, image: CoordinateImage, color):
+    step = abs(x1 - x0) > abs(y1 - y0)
+    if not step:
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
+    for x in range(x0, x1 + 1):
+        t = (x - x0) / (x1 - x0)
+        y = int(y0 * (1 - t) + y1 * t)
+        if step:
+            image.set(x, y, color)
+        else:
+            image.set(y, x, color)
+
+
+def line_4(x0, y0, x1, y1: int, image: CoordinateImage, color):
+    step = abs(x1 - x0) > abs(y1 - y0)
+    if not step:
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
+    dx = x1 - x0
+    dy = abs(y1 - y0)
+    de = dy / dx
+    err = 0.0
+    y = y0
+    y_direction = 1 if y1 > y0 else -1
+    for x in range(x0, x1 + 1):
+        if step:
+            image.set(x, y, color)
+        else:
+            image.set(y, x, color)
+        err += de
+        if err > 0.5:
+            y += y_direction
+            err -= 1
+
+
+def line_5(x0, y0, x1, y1: int, image: CoordinateImage, color):
+    step = abs(x1 - x0) > abs(y1 - y0)
+    if not step:
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
+    dx = x1 - x0
+    dy = abs(y1 - y0)
+    de = dy * 2
+    err = 0.0
+    y = y0
+    y_direction = 1 if y1 > y0 else -1
+    for x in range(x0, x1 + 1):
+        if step:
+            image.set(x, y, color)
+        else:
+            image.set(y, x, color)
+        err += de
+        if err > dx:
+            y += y_direction
+            err -= 2 * dx
--- test_line.py
+++ test_line.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import pytest
 
 import tga
@@ -16,3 +13,39 @@ def test_line_1(image):
     red = (255, 0, 0)
     line.line_1(13, 20, 80, 40, image, red)
     image.save("line_1")
+
+
+def test_line_2(image):
+    red = (255, 0, 0)
+    white = (255, 255, 255)
+    line.line_2(13, 20, 80, 40, image, white)
+    line.line_2(20, 13, 40, 80, image, red)
+    line.line_2(80, 40, 13, 20, image, red)
+    image.save("line_2")
+
+
+def test_line_3(image):
+    red = (255, 0, 0)
+    white = (255, 255, 255)
+    line.line_3(13, 20, 80, 40, image, white)
+    line.line_3(20, 13, 40, 80, image, red)
+    line.line_3(80, 40, 13, 20, image, red)
+    image.save("line_3")
+
+
+def test_line_4(image):
+    red = (255, 0, 0)
+    white = (255, 255, 255)
+    line.line_4(13, 20, 80, 40, image, white)
+    line.line_4(20, 13, 40, 80, image, red)
+    line.line_4(80, 40, 13, 20, image, red)
+    image.save("line_4")
+
+
+def test_line_5(image):
+    red = (255, 0, 0)
+    white = (255, 255, 255)
+    line.line_5(13, 20, 80, 40, image, white)
+    line.line_5(20, 13, 40, 80, image, red)
+    line.line_5(80, 40, 13, 20, image, red)
+    image.save("line_5")
